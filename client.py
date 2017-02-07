9d85b8dff7f177beea81be5d4258c9dee7137d95
Billy Schweigert <billyschweigert@gmail.com>
2017-02-07 14:37:50 -0500

    Uncommented rpio lines and removed input
    Rpio needs to be uncommented as it drives the pins that trigger the solid state relay. having an input statment in the while loop causes loop to hang when it should be executing based on commands.

--- client.py
+++ client.py
@@ -1,24 +1,22 @@
 #!/usr/bin/env python
 
 import socket
-##import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 
 server = socket.socket()
 host = socket.gethostbyaddr('192.168.1.104')
 port = 5150
 status = 'nominal'
-##GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BOARD)
 lights_pin = 12
-##GPIO.setup(lights_pin, GPIO.OUT)
-##GPIO.output(lights_pin,0)
+GPIO.setup(lights_pin, GPIO.OUT)
+GPIO.output(lights_pin,0)
 light_status = False;
 
 server.connect((host,port))
 data = server.recv(1024)
 print(bytes.decode(data))
 while True:
-    data = input('Enter Text')
-    server.send(str.encode(data))
     data = server.recv(1024)
     print('Recived from server',bytes.decode(data))
     if(bytes.decode(data) == 'exit'):
@@ -27,11 +25,11 @@ while True:
         server.send(str.encode(status))
     if(bytes.decode(data) == 'lights;'):
         if(not light_status):
-         ##   GPIO.output(lights_pin,True)
+            GPIO.output(lights_pin,True)
             print("on")
             light_status =True
         else:
-          ##  GPIO.output(lights_pin,False)
+            GPIO.output(lights_pin,False)
             print("off")
             light_status = False
             
